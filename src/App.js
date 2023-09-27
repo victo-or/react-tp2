754acd2c3c2925cc838c822c830f190de0bee410
victo-or <vic_cham@hotmail.com>
2023-09-26 23:03:47 -0400

    version 2 terminé

--- src/App.js
+++ src/App.js
@@ -1,93 +1,81 @@
-// import './App.css';
 import { useState, useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
 import ManyProducts from './components/ManyProducts'
 import AddProduct from './components/AddProduct'
 import Footer from './components/Footer'
-// import ModifProduct from './components/ModifProduct';
 import About from './components/About'
 
 function App() {
-  //GLOBAL
-  const [products, setProducts] = useState(
-    [
-      {
-        "id": 1,
-        "nom": "Bague en or 18 carats",
-        "description": "Bague sertie d'un diamant 1 carat de haute qualité.",
-        "prix": 1999.99,
-        "categorie": "Bijoux"
-      },
-      {
-        "id": 2,
-        "nom": "Téléviseur 4K Samsung",
-        "description": "Téléviseur intelligent avec une excellente qualité d'image.",
-        "prix": 699.99,
-        "categorie": "Électronique"
-      },
-      {
-        "id": 3,
-        "nom": "Guitare acoustique Martin D-28",
-        "description": "Guitare acoustique professionnelle en bois massif.",
-        "prix": 1699.99,
-        "categorie": "Instruments de Musique"
-      },
-      {
-        "id": 4,
-        "nom": "Perceuse à percussion Dewalt",
-        "description": "Perceuse à percussion électrique puissante pour les professionnels.",
-        "prix": 129.99,
-        "categorie": "Outils"
-      },
-      {
-        "id": 5,
-        "nom": "Bande dessinée Spider-Man #1",
-        "description": "Édition originale de Spider-Man datant de 1962.",
-        "prix": 499.99,
-        "categorie": "Articles de Collection"
-      }
-    ]
-  )
+  // État local pour stocker les produits récupérés depuis le serveur
+  const [products, setProducts] = useState([])
+
+  // Effet secondaire pour récupérer les produits depuis le serveur au chargement
+  useEffect(() =>{
+    const getProducts = async () => {
+        const productsFromServer = await fetchProducts()
+        setProducts(productsFromServer)
+    }
+    getProducts()
+  }, [])
+
+  // GLOBAL
+  // Fonction pour récupérer la liste des produits depuis le serveur
+  const fetchProducts = async () => {
+      const res = await fetch('http://localhost:5000/products')
+      const data = await res.json()
+      return data
+  } 
   
-  //DELETE
-  const deleteProduct = (id) => {
-    //console.log(id)
+  // DELETE
+  // Fonction pour supprimer un produit spécifique
+  const deleteProduct = async (id) => {
+    await fetch(`http://localhost:5000/products/${id}`, {
+      method: "DELETE",
+    });
+    // Mettre à jour l'état local en supprimant le produit
     setProducts(products.filter((product) => product.id !== id))
   }
-  //update
-  const updateProduct = (updatedProduct) => {
-    setProducts(products.map((product) => product.id === updatedProduct.id ? updatedProduct : product));
-  }
-  // setProducts(products.map((product) => product.id === id ? {...product, reminder:!product.reminder}: product ))
 
-  //Add
-  const addProduct = (product) => {
-    //console.log(product)
-    const id = Math.floor(Math.random() * 1000)
-    const newProduct = {id, ...product}
-    //console.log(newProduct)
+  // UPDATE
+  // Fonction pour mettre à jour un produit spécifique
+  const updateProduct = async (updatedProduct) => {
+    const res = await fetch(`http://localhost:5000/products/${updatedProduct.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedProduct),
+    })
+    // Mettre à jour l'état local des produits uniquement si la requête a réussi
+    setProducts(products.map((product) => (product.id === updatedProduct.id ? updatedProduct : product)));
+  }
+
+  // ADD
+  // Fonction pour ajouter un nouveau produit
+  const addProduct = async (product) => {
+    const res = await fetch('http://localhost:5000/products', {
+      method: 'post',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(product)
+    })
+    const newProduct = await res.json()
+    // Mettre à jour l'état local en ajoutant le nouveau produit
     setProducts([...products, newProduct])
    }
    
-  
-  // toggle form
+  // TOGGLE FORM
+  // État local pour contrôler l'affichage du formulaire d'ajout
   const [showAddProduct, setShowAddProduct] = useState(false)
-
-  // const location = useLocation();
 
   return (
   <BrowserRouter>
     <div className='container'>
+        {/* En-tête de l'application */}
         <Header onAddBtn={() => setShowAddProduct(!showAddProduct)} showAdd={showAddProduct}/>
-        {/* <AddProduct onAdd={addProduct}/> */}
         {showAddProduct && <AddProduct onAdd={addProduct}/>}
-        {/* <ManyProducts products={products} onDeleteMany={deleteProduct}  onToggleMany={toggleReminder} /> */}
-
-        {/* <ManyProducts products={products} onDeleteMany={deleteProduct} onUpdateMany={updateProduct}/> */}
-
-        {/* <ModifProduct/> */}
         <Routes>
           <Route path='/products' element={<ManyProducts products={products} onDeleteMany={deleteProduct} onUpdateMany={updateProduct} />}/>
         </Routes>
@@ -95,6 +83,7 @@ function App() {
           <Route path='/' element={<About/>}/>
         </Routes>
     </div>
+    {/* Pied de page de l'application */}
     <Footer/>
   </BrowserRouter>
   );
